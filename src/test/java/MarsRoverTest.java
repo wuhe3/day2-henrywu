627c342c772ea4555eaabb0edf7e503242a0014f
WUHE3 <wuhe3@oocl.com>
2024-11-19 17:00:53 +0800

    [MarsRover] test: add more testing case for turning left 1. should_face_east_when_orient_south_and_turn_left() 2. should_face_south_when_orient_west_and_turn_left() 3. should_face_east_when_orient_north_and_turn_right()

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -19,7 +19,6 @@ public class MarsRoverTest {
         assertEquals("0:0:N", report);
     }
 
-
     @Test
     public void should_face_west_when_orient_north_and_turn_left() {
         // Given
@@ -34,6 +33,48 @@ public class MarsRoverTest {
     }
 
     @Test
+    public void should_face_north_when_orient_east_and_turn_left() {
+        // Given
+        MarsRover rover = new MarsRover();
+
+        // When
+        rover.executeCommand('R');
+        rover.executeCommand('L');
+
+        // Then
+        assertEquals("0:0:N", rover.printReport());
+    }
+
+    @Test
+    public void should_face_east_when_orient_south_and_turn_left() {
+        // Given
+        MarsRover rover = new MarsRover();
+
+        // When
+        rover.executeCommand('R');
+        rover.executeCommand('R');
+        rover.executeCommand('L');
+
+        // Then
+        assertEquals("0:0:E", rover.printReport());
+    }
+
+    @Test
+    public void should_face_south_when_orient_west_and_turn_left() {
+        // Given
+        MarsRover rover = new MarsRover();
+
+        // When
+        rover.executeCommand('R');
+        rover.executeCommand('R');
+        rover.executeCommand('R');
+        rover.executeCommand('L');
+
+        // Then
+        assertEquals("0:0:S", rover.printReport());
+    }
+
+    @Test
     public void should_face_east_when_orient_north_and_turn_right() {
         // Given
         MarsRover rover = new MarsRover();
@@ -45,6 +86,8 @@ public class MarsRoverTest {
         // Then
         assertEquals("0:0:E", report);
     }
+
+
 
 
 }
