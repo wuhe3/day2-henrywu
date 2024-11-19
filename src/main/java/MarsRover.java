63a927142c9898013bf745dc2fd72358ad12e548
WUHE3 <wuhe3@oocl.com>
2024-11-19 15:41:51 +0800

    [MarsRover] feat, 'printReport' and initialize xAsis, yAsis, direction

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,7 +1,14 @@
 
 public class MarsRover {
 
+    private int xAsis = 0;
+    private int yAsis = 0;
+    private char direction = 'N';
 
+
+    public String printReport() {
+        return String.format("%d:%d:%c", xAsis, yAsis, direction);
+    }
 
 
 
