acddc2c22b910fc919ad443adbe509058e444a5d
WUHE3 <wuhe3@oocl.com>
2024-11-19 15:45:20 +0800

    [MarsRover] test, 'should_face_east_when_orient_north_and_turn_right'

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -33,5 +33,18 @@ public class MarsRoverTest {
         assertEquals("0:0:W", report);
     }
 
+    @Test
+    public void should_face_east_when_orient_north_and_turn_right() {
+        // Given
+        MarsRover rover = new MarsRover();
+
+        // When
+        rover.executeCommand('R');
+        String report = rover.printReport();
+
+        // Then
+        assertEquals("0:0:E", report);
+    }
+
 
 }
